65d7fd3f91b3e52d94d41a16da49aebd12f58e4c
80xer <80xer.s@gmail.com>
2021-06-17 22:59:35 +0900

    메인에 리스트 렌더링

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 const express = require('express');
+const { nanoid } = require('nanoid');
 const app = express();
 const port = 3000;
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Urls = mongoose.model('Urls', {
+  origin: String,
+  shrink: String,
+  createdAt: { type: Date, default: Date.now },
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.render('main');
+  Urls.find(function (err, data) {
+    res.render('main', { data });
+  });
 });
 
 app.post('/shrink', (req, res) => {
-  // 하나의 URL 쇼트너를 생성
+  const id = nanoid();
+  const url = new Urls({
+    origin: req.body.url,
+    shrink: id,
+  });
+  url.save().then((result) => {
+    console.log(`result`, result);
+  });
   res.redirect('/');
 });
 
